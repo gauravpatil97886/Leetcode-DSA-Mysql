7869521fae80df900ada717d2192c8e662774b71
Gaurav Patil <gauravpatil97886@gmail.com>
2023-09-24 10:36:53 +0530

    Time: 12 ms (35.25%), Space: 43.5 MB (10.14%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.cpp
+++ 0815-champagne-tower/0815-champagne-tower.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<double>> tower(101, vector<double>(101, 0.0));
         tower[0][0] = poured;
         
-        for (int i = 0; i < query_row; ++i) {
+        for (int i = 0; i <= query_row; ++i) {
             for (int j = 0; j <= i; ++j) {
                 double overflow = (tower[i][j] - 1.0) / 2.0;
                 if (overflow > 0) {
