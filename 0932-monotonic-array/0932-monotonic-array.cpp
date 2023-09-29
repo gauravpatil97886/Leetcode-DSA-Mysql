26f6c5034c228e5252c404cbf493724e70827647
Gaurav Patil <gauravpatil97886@gmail.com>
2023-09-29 11:21:37 +0530

    Time: 120 ms (67.15%), Space: 96.9 MB (13.85%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    bool isMonotonic(vector<int>& A) {
-        bool increase = true;
-        bool decrease = true;
-        for(int i = 0; i < A.size() - 1; i++) {
-            if(A[i] > A[i+1]) increase = false;
-            if(A[i] < A[i+1]) decrease = false;
-            if(increase == false && decrease == false) return false;
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true, decreasing = true;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            } else if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+            if (!increasing && !decreasing) return false;
         }
+
         return true;
     }
 };
