230976f9ecd767bff86ef7fddd17e33cd8dd4250
Gaurav Patil <gauravpatil97886@gmail.com>
2023-10-18 11:44:03 +0530

    Time: 1555 ms (13.24%), Space: 61.7 MB (27.65%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.py
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.py
@@ -1,25 +1,30 @@
+from collections import deque
+from typing import List
+import numpy as np
+
 class Solution:
-  def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-    graph = [[] for _ in range(n)]
-    inDegree = [0] * n
-    dist = time.copy()
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        graph = [[] for _ in range(n)]
+        inDegree = [0] * n
+        q = deque()
+        dist = np.array(time)
 
-    # Build graph.
-    for a, b in relations:
-      u = a - 1
-      v = b - 1
-      graph[u].append(v)
-      inDegree[v] += 1
+        for r in relations:
+            u = r[0] - 1
+            v = r[1] - 1
+            graph[u].append(v)
+            inDegree[v] += 1
 
-    # Topology
-    q = collections.deque([i for i, d in enumerate(inDegree) if d == 0])
+        for i in range(n):
+            if inDegree[i] == 0:
+                q.append(i)
 
-    while q:
-      u = q.popleft()
-      for v in graph[u]:
-        dist[v] = max(dist[v], dist[u] + time[v])
-        inDegree[v] -= 1
-        if inDegree[v] == 0:
-          q.append(v)
+        while q:
+            u = q.popleft()
+            for v in graph[u]:
+                dist[v] = max(dist[v], dist[u] + time[v])
+                inDegree[v] -= 1
+                if inDegree[v] == 0:
+                    q.append(v)
 
-    return max(dist)
+        return max(dist)
