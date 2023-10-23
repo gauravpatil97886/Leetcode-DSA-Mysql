00aa85d11e8a18eef1a1f71257c85f8cfec46ed2
Gaurav Patil <gauravpatil97886@gmail.com>
2023-10-23 11:15:39 +0530

    Time: 0 ms (100%), Space: 6.4 MB (26.74%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0){
-            return false;
-        }
-        else if(n==1){
-            return true;
-        }
-        return n%4==0 && isPowerOfFour(n/4);  
+        return (n > 0) && ((n & (n - 1)) == 0) && ((n & 0x55555555) == n);
     }
 };
