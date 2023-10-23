7b11761f4fac0d00f48649a18153c00889d46212
Gaurav Patil <gauravpatil97886@gmail.com>
2023-10-23 11:27:17 +0530

    Time: 5 ms (50.05%), Space: 14.6 MB (11.82%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -3,34 +3,34 @@ public:
     bool search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return true;
             }
-
-            if (nums[mid] == nums[left] && nums[right] == nums[mid]) {
+            if (nums[mid] == nums[left] && mid != left) {
                 left++;
-                continue;
             }
-
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && nums[mid] > target) {
+            if (nums[mid] == nums[right] && mid != right) {
+                right--;
+            }
+            else if (nums[mid] >= nums[left]) {
+                if (target < nums[mid] && target >= nums[left]) {
                     right = mid - 1;
-                } else {
+                }
+                else {
                     left = mid + 1;
                 }
-            } else {
-                if (nums[mid] < target && nums[right] >= target) {
+            }
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
-                } else {
+                }
+                else {
                     right = mid - 1;
                 }
             }
         }
-
         return false;
     }
 };
