d769b12fbb175e7cc740b890549eccb8121e5aa1
Gaurav Patil <gauravpatil97886@gmail.com>
2023-10-25 22:17:01 +0530

    Time: 3 ms (42.3%), Space: 6.4 MB (38.09%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,12 @@
 class Solution {
- public:
-  double myPow(double x, long n) {
-    if (n == 0)
-      return 1;
-    if (n < 0)
-      return 1 / myPow(x, -n);
-    if (n & 1)
-      return x * myPow(x, n - 1);
-    return myPow(x * x, n / 2);
-  }
+public:
+    double myPow(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return myPow(1 / x, -n);
+        }
+        return (n & 1) ? x * myPow(x * x, n / 2) : myPow(x * x, n / 2);
+    }
 };
