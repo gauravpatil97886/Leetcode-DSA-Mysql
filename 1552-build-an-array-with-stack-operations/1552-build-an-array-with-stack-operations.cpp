4c26de694ae6e689094ec99c09a0c4aa547e8949
Gaurav Patil <gauravpatil97886@gmail.com>
2023-11-03 08:56:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (49.67%) - LeetHub

---
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int i = 0; 
+        int num = 1; 
+
+        while (i < target.size()) {
+            int t = target[i];
+            if (t == num) {
+                ans.push_back("Push");
+                i++;
+            } else {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+            num++;
+        }
+
+        return ans;
+    }
+};
